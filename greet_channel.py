fa76605794f89a42610cc4d7492753c37c967353
sadeghinik <sadeghinik@gmail.com>
2026-09-01 15:02:21 +0330

    Add national code input to greet program

--- greet_channel.py
+++ greet_channel.py
@@ -1,15 +1,13 @@
 def main():
-    first_name = input("نام: ")
-    last_name = input("نام خانوادگی: ")
-    national_code = input("کد ملی: ")
+    first_name = input("نام را وارد کنید: ")
+    last_name = input("نام خانوادگی را وارد کنید: ")
+    national_code = input("کد ملی را وارد کنید: ")
 
     if not first_name or not last_name or not national_code:
-        print("لطفاً همه ورودی‌ها (نام، نام خانوادگی و کد ملی) را وارد کنید.")
+        print("لطفاً همه اطلاعات (نام، نام خانوادگی، کد ملی) را وارد کنید.")
         return
 
-    print(
-        f"{first_name} {last_name} ({national_code})"
-    )
+    print(f"{first_name} {last_name} ({national_code})")
 
 
 if __name__ == "__main__":
